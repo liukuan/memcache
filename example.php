c0f84bd44f3392755392f6c0a84a7b157a48cdb4
liukuan <unemail@qq.com>
2013-06-27 22:53:33 +0800

    Update example.php

--- example.php
+++ example.php
@@ -4,3 +4,9 @@
     $data = file_get_contents('http://www.facebook.com/');
     mcache::getInstance()->setFacebook($data);
   }else echo $content;
+
+
+  //Settings page content
+  $data = file_get_contents('http://www.facebook.com/?page=1');
+  mcache::getInstance()->setFacebook($data,1);
+  echo mcache::getInstance()->getFacebook(1);
