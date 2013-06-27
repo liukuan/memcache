06608aadec35c0d784bfc7d1983eb17378d49ed3
liukuan <unemail@qq.com>
2013-06-27 22:41:18 +0800

    Update example.php

--- example.php
+++ example.php
@@ -1,6 +1,5 @@
 <?php
   include 'mcache.php';
-  
   if(!$content = mcache::getInstance()->getFacebook()){
     $data = file_get_contents('http://www.facebook.com/');
     mcache::getInstance()->setFacebook($data);
