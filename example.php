e1a92a8874d1b01dff79f04dd6b15610f5f7ee81
liukuan <unemail@qq.com>
2013-06-27 23:04:59 +0800

    Update example.php

--- example.php
+++ example.php
@@ -8,7 +8,6 @@
   //delete
   mcache::getInstance()->delFacebook();
    
-   
   //Settings page content
   $data = file_get_contents('http://www.facebook.com/?page=1');
   mcache::getInstance()->setFacebook($data,1);
