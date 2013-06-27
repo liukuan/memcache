9e580b11e84f26a6d8dd4910873bba80ded217be
liukuan <unemail@qq.com>
2013-06-27 22:56:50 +0800

    Update example.php

--- example.php
+++ example.php
@@ -4,9 +4,19 @@
     $data = file_get_contents('http://www.facebook.com/');
     mcache::getInstance()->setFacebook($data);
   }else echo $content;
-
-
+  //delete
+  mcache::getInstance()->delFacebook();
+   
+   
   //Settings page content
   $data = file_get_contents('http://www.facebook.com/?page=1');
   mcache::getInstance()->setFacebook($data,1);
+  echo mcache::getInstance()->getFacebook(1);
+  
+  //replace
+  mcache::getInstance()->repFacebook($replace_str,1);
+  
+  //delete
+  mcache::getInstance()->delFacebook(1);
+  
   echo mcache::getInstance()->getFacebook(1);
