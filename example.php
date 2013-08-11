ac8943155ccd469c084f4c5024cc5e7277954646
root <root@192.168.1.105>
2013-08-11 18:50:20 +0800

    .....
    Change-Id: I9865e374e9d041ad6e6a43c3183e46fc9c1fdc0c

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
   include 'mcache.php';
-  
+  echo '...';
   //get content 
   if(!$content = mcache::getInstance()->getFacebook()){
     $data = file_get_contents('http://www.facebook.com/');
@@ -12,7 +12,7 @@
   mcache::getInstance()->repFacebook($data_str);
   //delete
   mcache::getInstance()->delFacebook();
-   
+   ###
   //Settings page content
   $data = file_get_contents('http://www.facebook.com/?page=1');
   mcache::getInstance()->setFacebook($data,1);
