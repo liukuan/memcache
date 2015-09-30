1a8d11416e24d21c324614bbe3631c9b6dac08a0
NewsBUZZ <lyamin2712@rambler.ru>
2015-09-30 16:47:21 +0300

    Update mcache.php

--- mcache.php
+++ mcache.php
@@ -2,7 +2,7 @@
 class mcache extends Memcache
 {
 	public $def_host = 'localhost'; 
-	public $def_port = '12000';
+	public $def_port = '11211';
 	public function __construct($host = null,$port = null){
 		$host = isset($host)?$host:$this->def_host;
 		$port = isset($port)?$port:$this->def_port;
